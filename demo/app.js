12745428cbe5b108dc0c9e2440a50b7606f43228
Tangrui Song <lamourthelove@gmail.com>
2015-11-11 00:14:16 -0800

    Finish Working With Models, Views, and Routes part

--- demo/app.js
+++ demo/app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.locals.appdata = require('./data.json');
 app.use('/', routes);
 
 // catch 404 and forward to error handler
--- demo/routes/index.js
+++ demo/routes/index.js
@@ -1,9 +1,68 @@
 var express = require('express');
 var router = express.Router();
+var appdata = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var myArtWork = [];
+    var myArtists = [];
+    myArtists = appdata.speakers;
+
+    appdata.speakers.forEach(function(speaker){
+        myArtWork = myArtWork.concat(speaker.artwork);
+
+    });
+    res.render('index', {
+        title: 'Home',
+        artwork: myArtWork,
+        artists: myArtists,
+        page: 'home'
+    });
 });
 
+/* GET all speakers' page. */
+router.get('/speakers', function(req, res, next) {
+    var myArtWork = [];
+    var myArtists = [];
+    myArtists = appdata.speakers;
+
+    appdata.speakers.forEach(function(speaker){
+        myArtWork = myArtWork.concat(speaker.artwork);
+    });
+    res.render('speakers', {
+        title: 'Speakers',
+        artwork: myArtWork,
+        artists: myArtists,
+        page: 'artistList'
+    });
+});
+
+/* GET a speaker detail page. */
+router.get('/speakers/:speakerid', function(req, res, next) {
+    var myArtWork = [];
+    var myArtists = [];
+
+    appdata.speakers.forEach(function(speaker){
+        if(speaker.shortname === req.params.speakerid) {
+            myArtists.push(speaker);
+            myArtWork = myArtWork.concat(speaker.artwork);
+        }
+    });
+    var allSpeakerNames = appdata.speakers.filter(function(item){
+        return item.shortname === req.params.speakerid;
+    });
+
+    var speakerName = "Speakers";
+    if(allSpeakerNames.length > 0){
+        speakerName = allSpeakerNames[0].name;
+    }
+
+    res.render('speakers', {
+        title: speakerName,
+        artwork: myArtWork,
+        artists: myArtists,
+        page: 'artistDetail'
+    });
+
+});
 module.exports = router;
